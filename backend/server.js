a4128b418bde80d453d534f7d04d4a797091bede
Homesh Nimbarte <75970868+resonating-homesh@users.noreply.github.com>
2023-01-16 18:56:45 +0530

    added websocket functionality

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,23 @@
-// import { chats } from "./data/data";
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
 const express = require("express");
 const connectDB = require("./config/db");
 const { chats } = require("./data/data");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes");
 
 dotenv.config();
 connectDB();
 const app = express();
-const {notFound, errorHandler} = require('../backend/middleware/errorMiddleware');
+const {
+  notFound,
+  errorHandler,
+} = require("../backend/middleware/errorMiddleware");
 
 app.use(express.json()); //this tell react app to use json data
 
-app.get('/', (req, res)=>{
-    res.send("API is running");
+app.get("/", (req, res) => {
+  res.send("API is running");
 });
 
 // app.get('/api/chat', (req, res) =>
@@ -22,19 +25,59 @@ app.get('/', (req, res)=>{
 //     res.send(chats);
 // });
 
-app.get('/api/chat/:id',(req, res) =>
-{
-    console.log(req.params.id);
-    const singleChat = chats.find( (c) => c._id === req.params.id);
-    res.send(singleChat);
+app.get("/api/chat/:id", (req, res) => {
+  console.log(req.params.id);
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
 });
 
-app.use('/api/user', userRoutes);
-app.use('/api/chat', chatRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log("hello there!"))
+const server = app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
+
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("connected to socket.io");
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User Joined the Room: " + room);
+  });
+
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+  socket.on("new message", (newMessageRecieved) => {
+    var chat = newMessageRecieved.chat;
+    console.log("new message", newMessageRecieved);
+    if (!chat.users) return console.log("chat.users not defined");
+
+    chat.users.forEach((user) => {
+      if (user._id == newMessageRecieved.sender._id) return;
+
+      socket.in(user._id).emit("message recieved", newMessageRecieved);
+    });
+  });
+
+  socket.off("setup", () => {
+    console.log("User Disconneted");
+    socket.leave(userData._id);
+  });
+});
